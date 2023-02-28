da7319df7935904b075d4197e7523459110bb81b
udaysaigalla <udaysaigalla8@gmail.com>
2023-02-28 22:49:22 +0530

    Welcome to EmpWageComputation

---
+++ EmpWageComputation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmpwageComputation");
+        }
+    }
+}
